5155ef4e8a7e10f4154c639dcdb6a4b0d2413b89
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-12-21 19:26:25 +0100

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-const { ListNode } = require('../extensions/list-node.js');
+// const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,39 +13,42 @@ const { ListNode } = require('../extensions/list-node.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
-  constructor() {
-    this.head = null;
-    this.size = 0;
+ class ListNode {
+  constructor(x) {
+    this.value = x;
+    this.next = null;
   }
+}
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
+let list = []
+
+class Queue {
 
   getUnderlyingList() {
-    return this.head;
+    list.splice(0, 1)
+    return convertArrayToList(list)
   }
 
   enqueue(value) {
-    if (this.size === 0) {
-      this.head = new ListNode(value);
-    } else {
-      let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-
-      current.next = new ListNode(value);
-    }
-
-    this.size++;
+    list.push(value)
   }
 
   dequeue() {
-    let current = this.head.value;
-    this.head = this.head.next;
-    this.size -= 1;
-    return current;
+    let x = list[0]
+    list.splice(0, 1)
+    return x
   }
 }
-
 module.exports = {
-  Queue,
+  Queue
 };
